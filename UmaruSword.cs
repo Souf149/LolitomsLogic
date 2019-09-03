10ce5d1ac107a889dffc7765517161110f6d5078
Soufyan <MetaByte149@gmail.com>
2019-09-03 20:28:15 +0200

    added projectile

--- UmaruSword.cs
+++ UmaruSword.cs
@@ -13,7 +13,7 @@ namespace LolitomsLogic
         }
 
         public override void SetDefaults() {
-            item.damage = 100;            
+            item.damage = 200;            
             item.melee = true;          
             item.width = 40;           
             item.height = 40;          
@@ -24,7 +24,11 @@ namespace LolitomsLogic
             item.value = Item.buyPrice(gold: 1);          
             item.rare = 13;             
             item.UseSound = SoundID.Item1;     
-            item.autoReuse = true;         
+            item.autoReuse = true;
+
+            // projectile
+            item.shoot = 9; // star fury star
+            item.shootSpeed = 8f;
         }
 
         public override void AddRecipes() {
@@ -41,6 +45,39 @@ namespace LolitomsLogic
                 Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("UmaruDust"));
             }
         }
+
+        // Star Wrath/Starfury style weapon. Spawn projectiles from sky that aim towards mouse.
+        // See Source code for Star Wrath projectile to see how it passes through tiles.
+		 	
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+			Vector2 target = Main.screenPosition + new Vector2((float)Main.mouseX, (float)Main.mouseY);
+			float ceilingLimit = target.Y;
+			if (ceilingLimit > player.Center.Y - 200f)
+			{
+				ceilingLimit = player.Center.Y - 200f;
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				position = player.Center + new Vector2((-(float)Main.rand.Next(0, 401) * player.direction), -600f);
+				position.Y -= (100 * i);
+				Vector2 heading = target - position;
+				if (heading.Y < 0f)
+				{
+					heading.Y *= -1f;
+				}
+				if (heading.Y < 20f)
+				{
+					heading.Y = 20f;
+				}
+				heading.Normalize();
+				heading *= new Vector2(speedX, speedY).Length();
+				speedX = heading.X;
+				speedY = heading.Y + Main.rand.Next(-40, 41) * 0.02f;
+				Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage * 2, knockBack, player.whoAmI, 0f, ceilingLimit);
+			}
+			return false;
+		}
+
 
 
     }
