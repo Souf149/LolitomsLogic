43979ba825a6f335f7d48f800b347a642d30a9d9
Soufyan <MetaByte149@gmail.com>
2019-09-03 16:40:31 +0200

    Added particles to sword

--- UmaruDust.cs
+++ UmaruDust.cs
@@ -3,21 +3,19 @@ using Terraria;
 using Terraria.ModLoader;
 
 namespace LolitomsLogic {
-    class UmaruDust : ModDust {
+    public class UmaruDust : ModDust {
         public override void OnSpawn(Dust dust) {
-            dust.velocity *= 0.4f;
-            dust.noGravity = true;
-            dust.noLight = true;
-            dust.scale *= 1.5f;
+            dust.velocity *= 1.4f;
+            dust.noGravity = false;
+            dust.frame = new Rectangle(0, 0, 16, 16);
         }
 
         public override bool Update(Dust dust) {
-            dust.position += dust.velocity;
-            dust.rotation += dust.velocity.X * 0.15f;
-            dust.scale *= 0.99f;
-            float light = 0.35f * dust.scale;
-            Lighting.AddLight(dust.position, light, light, light);
-            if (dust.scale < 0.5f) {
+            // dust.position += dust.velocity;
+            dust.position.Y += 5;
+            dust.rotation += 0.1f;
+            dust.scale -= 0.01f;
+            if (dust.scale < 0.9f) {
                 dust.active = false;
             }
             return false;
